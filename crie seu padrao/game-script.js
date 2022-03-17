f5816cab0c634daa1a457a6ae072fdea9a51345a
Henrique Bandeira <henriquebandeira307@gmail.com>
2022-03-17 18:50:10 -0300

    Criar sequencia - Corrigindo bugs variados

--- crie seu padrao/game-script.js
+++ crie seu padrao/game-script.js
@@ -36,7 +36,7 @@ var arraySequencia = []; //Array para guardar a sequecia
 var arrayOpcoes = []; //Array contendo todos os elementos gerados nas opcoes
 var tamNucleo; //Quantos elementos o nucleo possui
 var tamSeq = 0; //Tamanho da sequência do núcleo
-var etapaAtual = 16;
+var etapaAtual = 0;
 var cliquei = false; // var que verifica se a crianca ja viu a sequencia que escolheu
 var estrela = 0; //nível de estrelas do jogador 
 var endGame = false; //Indica se o jogo está na ultima fase
