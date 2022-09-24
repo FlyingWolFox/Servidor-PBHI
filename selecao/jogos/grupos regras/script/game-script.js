c1bc6b3b849421557408e2ac03010654d2becebd
flyingwolfox <49326973+FlyingWolFox@users.noreply.github.com>
2022-09-24 12:28:46 -0300

    removidas funções de operações em sets
    devido a falta de uso de sets no código, essas funções não são mais necessárias

--- selecao/jogos/grupos regras/script/game-script.js
+++ selecao/jogos/grupos regras/script/game-script.js
@@ -920,33 +920,6 @@ function stopChuva() {
     });
 }
 
-function setDifference(setA, setB) {
-    const _difference = new Set(setA);
-    for (const elem of setB) {
-        _difference.delete(elem);
-    }
-    return _difference;
-}
-
-// TODO: replace with a better implementation
-function setIntersection(setA, setB) {
-    const _intersection = new Set();
-    for (const elem of setB) {
-        if (setA.has(elem)) {
-            _intersection.add(elem);
-        }
-    }
-    return _intersection;
-}
-
-function setUnion(setA, setB) {
-    const _union = new Set(setA);
-    for (const elem of setB) {
-        _union.add(elem);
-    }
-    return _union;
-}
-
 function shuffleArray(array) {
     /* Randomize array in-place using Durstenfeld shuffle algorithm */
     for (let i = array.length - 1; i > 0; i--) {
