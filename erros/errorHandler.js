f6ba12e49ca2748e4d785bd1ca7ed6ad3a21bba8
Joao Vittor Souza <joaovittor@eng.ci.ufpb.br>
2023-03-08 12:08:55 -0300

    adicionando erros especificos de banco TODO:tratar todos os codigos de bd

--- erros/errorHandler.js
+++ erros/errorHandler.js
@@ -13,6 +13,14 @@ const errorHandler = (err, req, res, next) => {
     if (err instanceof ValidationError) {
         return res.status(err.statusCode).json(err.message);
       }
+    if (err.code === 'ER_DUP_ENTRY' || err.code === 'ER_BAD_NULL_ERROR' ) { // Erro de chave duplicada ou falta de valor em campo not null
+      // comentando aqui para continuar o desenvolvimento dessa parte do projeto
+      //fica faltando o tratamento de erros específicos do banco, esses erros são lançados pelo banco de dados e não pelo código, por isso não são instâncias de AppError ou ValidationError
+      // ainda estou pensando em como fazer isso
+        console.log("Erro de banco específico:" + err.code)
+        return res.status(500).send('Erro de banco de dados');
+     }
+      console.log("Erro não tratado: " + err.message)
       return res.status(500).send("Algo deu errado");
 };
 
