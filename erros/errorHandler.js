591de9e8e780c495c6aaa6557058be171812a6a2
Joao Vittor Souza <joaovittor@eng.ci.ufpb.br>
2023-03-10 02:09:25 -0300

    Adicionando tratamento dos erros de nomenclatura do fluxograma

--- erros/errorHandler.js
+++ erros/errorHandler.js
@@ -13,15 +13,22 @@ const errorHandler = (err, req, res, next) => {
     if (err instanceof ValidationError) {
         return res.status(err.statusCode).json(err.message);
       }
-    if (err.code === 'ER_DUP_ENTRY' || err.code === 'ER_BAD_NULL_ERROR' ) { // Erro de chave duplicada ou falta de valor em campo not null
+      if(err.code === 'ER_WARN_DATA_TRUNCATED'){ //erro causado pelo jogo fluxograma com os nodes em tela reduzida, o texto fica menor que o salvo na enum do banco
+        // todo: adicionar o texto minimizado na enum do banco
+        console.log('Erro do fluxograma: ' + err.code);
+        return res.status(200).json('Ok');
+      }
+    if (err.code === 'ER_DUP_ENTRY' || err.code === 'ER_BAD_NULL_ERROR') { // Erro de chave duplicada ou falta de valor em campo not null
       // comentando aqui para continuar o desenvolvimento dessa parte do projeto
       //fica faltando o tratamento de erros específicos do banco, esses erros são lançados pelo banco de dados e não pelo código, por isso não são instâncias de AppError ou ValidationError
       // ainda estou pensando em como fazer isso
         console.log("Erro de banco específico:" + err.code)
         return res.status(500).send('Erro de banco de dados');
-     }
-      console.log("Erro não tratado: " + err.message)
+     }else{
+      console.log("Erro não tratado: " + err)
       return res.status(500).send("Algo deu errado");
+     }
+      
 };
 
 module.exports = errorHandler;
